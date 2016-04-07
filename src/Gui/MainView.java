5bd885d749138706c4f7216ad58a8bd6a7afb51d
Coweete <Jonatan_fridsten@hotmail.com>
2016-04-07 17:18:39 +0200

    First setup

---
+++ src/Gui/MainView.java
@@ -0,0 +1,29 @@
+package Gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by jonatan on 2016-04-06.
+ */
+public class MainView extends Application {
+
+
+    private Scene scene;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        scene = new Scene(root, 480,320);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String [] args){
+        launch(args);
+    }
+}
